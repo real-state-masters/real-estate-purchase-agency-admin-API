cdd765d29a2b4d032207394450ce5355c012376c
Ismael Vazquez <ismael.vazquez.ro@gmail.com>
2021-02-08 17:58:15 +0100

    test_structure done

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,55 +15,58 @@ class ExampleTest extends TestCase
      */
     public function test_get()
     {
-        $response = $this->get('/api/properties/?jwt='.env('CLIENT_TOKEN'));
+        $response = $this->get('/api/properties/?jwt=' . env('CLIENT_TOKEN'));
 
         $response->assertStatus(200);
     }
 
+
+
+
     public function test_structure()
     {
-        $response = $this->get('/api/properties/1612798145602158c179875?jwt='.env('CLIENT_TOKEN'));
+        $response = $this->get('/api/properties?jwt='.env('CLIENT_TOKEN'));
 
-        $property = $response->getData();
-        //dd($response);
-        $response->assertJsonStructure([
-                'success',
-                'message',
-                'data' =>[
-                '_id',
-                'location', // type: object
-                'location' => [
-                    'id',
-                    'coordinates',
-                    'address',
-                    'context',
-                    'property_id',
-                ],
-                'type', // type: enum  (home/office)
-                'type_house', // type: int, -1-> not a house,  0 -> duplex, 1->house, 2->penthouse
-                'area', // type int ( m^²)
-                'status' , // type: boolean .  true -> not sold, false-> sold
-                'bought_by', // type: int ( user_id).  if -1-> not bought by anyone
-                'created_at',
-                'updated_at',
-                'price', // type: int
-                'images', // array of url's  // type: array
-                'description', // type:string
-                'num_bathrooms', // type: int
-                'num_rooms', // type: int
-                'pets', // type: bool
-                'equipment', //  type: int 0-> Indifferent , 1-> fully fitted kitchen, 2-> furnished
-                'garden', // type:bool
-                'swimming_pool', // type: bool
-                'lift', // type: bool
-                'condition', // type: int , 0-> new homes, 1-> good condition , 2-> needs renovation
-                'air_condition', // type: bool
-                'terrace', // type: bool
-                'contact', // admin email
-                'title', // type: string
-                'building_use'  // type:integer, -1-> not an office,  0-> private, 1->co_working , 2-> security_system
+        $a = sizeOf($response->getData());
+
+        for ($i = 0; $i < $a; $i++) {
+            $response->assertJsonStructure(
+                [
+                    $i => [
+                        '_id',
+                        'location' => [
+                            'id',
+                            'coordinates',
+                            'address',
+                            'context',
+                            'property_id',
+                        ],
+                        'type', // type: enum  (home/office)
+                        'type_house', // type: int, -1-> not a house,  0 -> duplex, 1->house, 2->penthouse
+                        'area', // type int ( m^²)
+                        'status', // type: boolean .  true -> not sold, false-> sold
+                        'bought_by', // type: int ( user_id).  if -1-> not bought by anyone
+                        'created_at',
+                        'updated_at',
+                        'price', // type: int
+                        'images', // array of url's  // type: array
+                        'description', // type:string
+                        'num_bathrooms', // type: int
+                        'num_rooms', // type: int
+                        'pets', // type: bool
+                        'equipment', //  type: int 0-> Indifferent , 1-> fully fitted kitchen, 2-> furnished
+                        'garden', // type:bool
+                        'swimming_pool', // type: bool
+                        'lift', // type: bool
+                        'condition', // type: int , 0-> new homes, 1-> good condition , 2-> needs renovation
+                        'air_condition', // type: bool
+                        'terrace', // type: bool
+                        'contact', // admin email
+                        'title', // type: string
+                        'building_use'  // type:integer, -1-> not an office,  0-> private, 1->co_working , 2-> security_system
+                    ]
                 ]
-                ]);
-
+            );
+        }
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,6 +11,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+
     public function testBasicTest()
     {
         $this->assertTrue(true);
